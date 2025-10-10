a1f94927d5bb1e00420dedea522c5831919081d2
CoreXChain <contact@corexkey.com>
2025-10-10 15:20:00 +0800

    add node, rpc and runtime interface definitions

---
+++ node/api.go
@@ -0,0 +1,385 @@
+// Copyright 2015 CoreX Team
+// This file is part of the corex-chain library.
+//
+// The corex-chain library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The corex-chain library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the corex-chain library. If not, see <http://www.gnu.org/licenses/>.
+
+package node
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"com.corexkey/common/hexutil"
+	"com.corexkey/crypto"
+	"com.corexkey/internal/debug"
+	"com.corexkey/log"
+	"com.corexkey/p2p"
+	"com.corexkey/p2p/enode"
+	"com.corexkey/rpc"
+)
+
+// apis returns the collection of built-in RPC APIs.
+func (n *Node) apis() []rpc.API {
+	return []rpc.API{
+		{
+			Namespace: "admin",
+			Service:   &adminAPI{n},
+		}, {
+			Namespace: "control",
+			Service:   &adminAPI{n},
+		}, {
+			Namespace: "debug",
+			Service:   debug.Handler,
+		}, {
+			Namespace: "identity",
+			Service:   &web3API{n},
+		},
+	}
+}
+
+// adminAPI is the collection of administrative API methods exposed over
+// both secure and unsecure RPC channels.
+type adminAPI struct {
+	node *Node // Node interfaced by this API
+}
+
+// AddPeer requests connecting to a remote node, and also maintaining the new
+// connection at all times, even reconnecting if it is lost.
+func (api *adminAPI) AddPeer(url string) (bool, error) {
+	// Make sure the server is running, fail otherwise
+	server := api.node.Server()
+	if server == nil {
+		return false, ErrNodeStopped
+	}
+	// Try to add the url as a static peer and return
+	node, err := enode.Parse(enode.ValidSchemes, url)
+	if err != nil {
+		return false, fmt.Errorf("invalid enode: %v", err)
+	}
+	server.AddPeer(node)
+	return true, nil
+}
+
+// JoinPeer requests connecting to a remote node and maintaining the link.
+func (api *adminAPI) JoinPeer(url string) (bool, error) {
+	return api.AddPeer(url)
+}
+
+// RemovePeer disconnects from a remote node if the connection exists
+func (api *adminAPI) RemovePeer(url string) (bool, error) {
+	// Make sure the server is running, fail otherwise
+	server := api.node.Server()
+	if server == nil {
+		return false, ErrNodeStopped
+	}
+	// Try to remove the url as a static peer and return
+	node, err := enode.Parse(enode.ValidSchemes, url)
+	if err != nil {
+		return false, fmt.Errorf("invalid enode: %v", err)
+	}
+	server.RemovePeer(node)
+	return true, nil
+}
+
+// DropPeer disconnects from a remote node if the connection exists.
+func (api *adminAPI) DropPeer(url string) (bool, error) {
+	return api.RemovePeer(url)
+}
+
+// AddTrustedPeer allows a remote node to always connect, even if slots are full
+func (api *adminAPI) AddTrustedPeer(url string) (bool, error) {
+	// Make sure the server is running, fail otherwise
+	server := api.node.Server()
+	if server == nil {
+		return false, ErrNodeStopped
+	}
+	node, err := enode.Parse(enode.ValidSchemes, url)
+	if err != nil {
+		return false, fmt.Errorf("invalid enode: %v", err)
+	}
+	server.AddTrustedPeer(node)
+	return true, nil
+}
+
+// JoinTrustedPeer marks a peer as trusted for CoreXChain coordination.
+func (api *adminAPI) JoinTrustedPeer(url string) (bool, error) {
+	return api.AddTrustedPeer(url)
+}
+
+// RemoveTrustedPeer removes a remote node from the trusted peer set, but it
+// does not disconnect it automatically.
+func (api *adminAPI) RemoveTrustedPeer(url string) (bool, error) {
+	// Make sure the server is running, fail otherwise
+	server := api.node.Server()
+	if server == nil {
+		return false, ErrNodeStopped
+	}
+	node, err := enode.Parse(enode.ValidSchemes, url)
+	if err != nil {
+		return false, fmt.Errorf("invalid enode: %v", err)
+	}
+	server.RemoveTrustedPeer(node)
+	return true, nil
+}
+
+// DropTrustedPeer removes a trusted peer marker.
+func (api *adminAPI) DropTrustedPeer(url string) (bool, error) {
+	return api.RemoveTrustedPeer(url)
+}
+
+// PeerEvents creates an RPC subscription which receives peer events from the
+// node's p2p.Server
+func (api *adminAPI) PeerEvents(ctx context.Context) (*rpc.Subscription, error) {
+	// Make sure the server is running, fail otherwise
+	server := api.node.Server()
+	if server == nil {
+		return nil, ErrNodeStopped
+	}
+
+	// Create the subscription
+	notifier, supported := rpc.NotifierFromContext(ctx)
+	if !supported {
+		return nil, rpc.ErrNotificationsUnsupported
+	}
+	rpcSub := notifier.CreateSubscription()
+
+	go func() {
+		events := make(chan *p2p.PeerEvent)
+		sub := server.SubscribeEvents(events)
+		defer sub.Unsubscribe()
+
+		for {
+			select {
+			case event := <-events:
+				notifier.Notify(rpcSub.ID, event)
+			case <-sub.Err():
+				return
+			case <-rpcSub.Err():
+				return
+			case <-notifier.Closed():
+				return
+			}
+		}
+	}()
+
+	return rpcSub, nil
+}
+
+// CommunicationEvents streams peer events from the communication layer.
+func (api *adminAPI) CommunicationEvents(ctx context.Context) (*rpc.Subscription, error) {
+	return api.PeerEvents(ctx)
+}
+
+// StartHTTP starts the HTTP RPC API server.
+func (api *adminAPI) StartHTTP(host *string, port *int, cors *string, apis *string, vhosts *string) (bool, error) {
+	api.node.lock.Lock()
+	defer api.node.lock.Unlock()
+
+	// Determine host and port.
+	if host == nil {
+		h := DefaultHTTPHost
+		if api.node.config.HTTPHost != "" {
+			h = api.node.config.HTTPHost
+		}
+		host = &h
+	}
+	if port == nil {
+		port = &api.node.config.HTTPPort
+	}
+
+	// Determine config.
+	config := httpConfig{
+		CorsAllowedOrigins: api.node.config.HTTPCors,
+		Vhosts:             api.node.config.HTTPVirtualHosts,
+		Modules:            api.node.config.HTTPModules,
+		rpcEndpointConfig: rpcEndpointConfig{
+			batchItemLimit:         api.node.config.BatchRequestLimit,
+			batchResponseSizeLimit: api.node.config.BatchResponseMaxSize,
+		},
+	}
+	if cors != nil {
+		config.CorsAllowedOrigins = nil
+		for _, origin := range strings.Split(*cors, ",") {
+			config.CorsAllowedOrigins = append(config.CorsAllowedOrigins, strings.TrimSpace(origin))
+		}
+	}
+	if vhosts != nil {
+		config.Vhosts = nil
+		for _, vhost := range strings.Split(*host, ",") {
+			config.Vhosts = append(config.Vhosts, strings.TrimSpace(vhost))
+		}
+	}
+	if apis != nil {
+		config.Modules = nil
+		for _, m := range strings.Split(*apis, ",") {
+			config.Modules = append(config.Modules, strings.TrimSpace(m))
+		}
+	}
+
+	if err := api.node.http.setListenAddr(*host, *port); err != nil {
+		return false, err
+	}
+	if err := api.node.http.enableRPC(api.node.rpcAPIs, config); err != nil {
+		return false, err
+	}
+	if err := api.node.http.start(); err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// StartRPC starts the HTTP RPC API server.
+// Deprecated: use StartHTTP instead.
+func (api *adminAPI) StartRPC(host *string, port *int, cors *string, apis *string, vhosts *string) (bool, error) {
+	log.Warn("Deprecation warning", "method", "admin.StartRPC", "use-instead", "admin.StartHTTP")
+	return api.StartHTTP(host, port, cors, apis, vhosts)
+}
+
+// StopHTTP shuts down the HTTP server.
+func (api *adminAPI) StopHTTP() (bool, error) {
+	api.node.http.stop()
+	return true, nil
+}
+
+// StopRPC shuts down the HTTP server.
+// Deprecated: use StopHTTP instead.
+func (api *adminAPI) StopRPC() (bool, error) {
+	log.Warn("Deprecation warning", "method", "admin.StopRPC", "use-instead", "admin.StopHTTP")
+	return api.StopHTTP()
+}
+
+// StartWS starts the websocket RPC API server.
+func (api *adminAPI) StartWS(host *string, port *int, allowedOrigins *string, apis *string) (bool, error) {
+	api.node.lock.Lock()
+	defer api.node.lock.Unlock()
+
+	// Determine host and port.
+	if host == nil {
+		h := DefaultWSHost
+		if api.node.config.WSHost != "" {
+			h = api.node.config.WSHost
+		}
+		host = &h
+	}
+	if port == nil {
+		port = &api.node.config.WSPort
+	}
+
+	// Determine config.
+	config := wsConfig{
+		Modules: api.node.config.WSModules,
+		Origins: api.node.config.WSOrigins,
+		// ExposeAll: api.node.config.WSExposeAll,
+		rpcEndpointConfig: rpcEndpointConfig{
+			batchItemLimit:         api.node.config.BatchRequestLimit,
+			batchResponseSizeLimit: api.node.config.BatchResponseMaxSize,
+		},
+	}
+	if apis != nil {
+		config.Modules = nil
+		for _, m := range strings.Split(*apis, ",") {
+			config.Modules = append(config.Modules, strings.TrimSpace(m))
+		}
+	}
+	if allowedOrigins != nil {
+		config.Origins = nil
+		for _, origin := range strings.Split(*allowedOrigins, ",") {
+			config.Origins = append(config.Origins, strings.TrimSpace(origin))
+		}
+	}
+
+	// Enable WebSocket on the server.
+	server := api.node.wsServerForPort(*port, false)
+	if err := server.setListenAddr(*host, *port); err != nil {
+		return false, err
+	}
+	openApis, _ := api.node.getAPIs()
+	if err := server.enableWS(openApis, config); err != nil {
+		return false, err
+	}
+	if err := server.start(); err != nil {
+		return false, err
+	}
+	api.node.http.log.Info("WebSocket endpoint opened", "url", api.node.WSEndpoint())
+	return true, nil
+}
+
+// StopWS terminates all WebSocket servers.
+func (api *adminAPI) StopWS() (bool, error) {
+	api.node.http.stopWS()
+	api.node.ws.stop()
+	return true, nil
+}
+
+// Peers retrieves all the information we know about each individual peer at the
+// protocol granularity.
+func (api *adminAPI) Peers() ([]*p2p.PeerInfo, error) {
+	server := api.node.Server()
+	if server == nil {
+		return nil, ErrNodeStopped
+	}
+	return server.PeersInfo(), nil
+}
+
+// NodeInfo retrieves all the information we know about the host node at the
+// protocol granularity.
+func (api *adminAPI) NodeInfo() (*p2p.NodeInfo, error) {
+	server := api.node.Server()
+	if server == nil {
+		return nil, ErrNodeStopped
+	}
+	return server.NodeInfo(), nil
+}
+
+// NodeProfile retrieves the host node profile.
+func (api *adminAPI) NodeProfile() (*p2p.NodeInfo, error) {
+	return api.NodeInfo()
+}
+
+// Datadir retrieves the current data directory the node is using.
+func (api *adminAPI) Datadir() string {
+	return api.node.DataDir()
+}
+
+// Workspace retrieves the current data directory used by the node.
+func (api *adminAPI) Workspace() string {
+	return api.Datadir()
+}
+
+// web3API offers helper utils
+type web3API struct {
+	stack *Node
+}
+
+// ClientVersion returns the node name
+func (s *web3API) ClientVersion() string {
+	return s.stack.Server().Name
+}
+
+// CoreXVersion returns the CoreXChain node identity string.
+func (s *web3API) CoreXVersion() string {
+	return s.ClientVersion()
+}
+
+// Sha3 applies the corexchain sha3 implementation on the input.
+// It assumes the input is hex encoded.
+func (s *web3API) Sha3(input hexutil.Bytes) hexutil.Bytes {
+	return crypto.Keccak256(input)
+}
+
+// Digest applies the CoreXChain digest function on the input.
+func (s *web3API) Digest(input hexutil.Bytes) hexutil.Bytes {
+	return s.Sha3(input)
+}
